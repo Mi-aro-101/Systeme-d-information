48a946d5d1cae7d5063910af8ed0a6cfe24cd389
root <root@Ramanana.Ramanana>
2023-03-11 09:32:13 -0500

    update insert comptable

---
+++ application/controllers/Insert_comptable.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Insert_comptable extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('Insert_Comptable');
+	}
+}
