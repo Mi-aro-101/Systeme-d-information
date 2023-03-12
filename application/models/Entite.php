73f9ad4498815280d7873cd0f08ab374a6521480
MamyAndry <aosimamamisoa@gmail.com>
2023-03-12 17:17:31 +0300

    Modif model Entite,view Formulaire & ajout controller FormEntite

--- application/models/Entite.php
+++ application/models/Entite.php
@@ -10,17 +10,33 @@ class Entite extends CI_Model {
         $value = $query->row_arrray();
         return $value['currval'];
     }
+    public function getNextYearDate($date){
+        list($year,$month,$day) = explode("-",$date);
+        $year = "".intval($year) + 1;
+        if($month == '02' || $month == '2' && $day == '29'){
+            $month = '03';
+            $day = '01';
+        }
+        return $year.$month.$day;
+    }
     public function insertEntite($nomFondateur,$nomSociete,$numFisc,$siege,$dateCreation,$dateDebut,$objet,$numStat,$numReg,$deviseTC,$deviseEq,$pwd)
 	{
-        $sql = "INSERT INTO Entite VALUES(default,'%s','%s')";
-        $sql = sprintf($sql,$nomSociete,$pwd);
-        $this->db->query($sql);
-        $id = getCurrval();
-        $sql = "INSERT INTO details VALUES(%i,'%s','%s','%s','%s','%s','%s','%s','%s')";
-        $sql = sprintf($sql,$id,$val,$nomFondateur,$numFisc,$siege,$dateCreation,$objet,$numStat,$deviseTC,$deviseEq);
-        $this->db->query($sql);
-        $sql = "INSERT INTO exercice VALUES(default,'%s','%s')";
-        $sql = sprintf($sql,);
-        $this->db->query($sql);
+        try{             
+            $sql = "BEGIN;INSERT INTO Entite VALUES(default,'%s','%s')";
+            $sql = sprintf($sql,$nomSociete,$pwd);
+            $this->db->query($sql);
+            $id = getCurrval();
+            $sql = "INSERT INTO details VALUES(%i,'%s','%s','%s','%s','%s','%s','%s','%s')";
+            $sql = sprintf($sql,$id,$val,$nomFondateur,$numFisc,$siege,$dateCreation,$objet,$numStat,$deviseTC,$deviseEq);
+            $this->db->query($sql);
+            $sql = "INSERT INTO exercice VALUES(default,'%s','%s')";
+            $sql = sprintf($sql,$dateDebut,getNextYearDate($dateDebut));
+            $this->db->query($sql);
+            $this->db->query("COMMIT");
+        }
+        catch (Exception $e) {
+            $this->db->query("ROLLBACK");  
+            throw $e;
+        }
     }
 }
