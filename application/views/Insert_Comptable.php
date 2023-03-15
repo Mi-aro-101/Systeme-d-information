2ebe250a5b2bd8c87e201e701fa77b67168f1199
MamyAndry <119005054+MamyAndry@users.noreply.github.com>
2023-03-15 19:55:46 +0300

    Update view Insert_comptable

--- application/views/Insert_Comptable.php
+++ application/views/Insert_Comptable.php
@@ -9,36 +9,42 @@
     <link rel="stylesheet" href="<?php echo base_url('css/bootstrap.min.css'); ?>">
 </head>
 <body>
-    <form action="<?php echo base_url('index.php/Insert_comptable/Insert'); ?>" method="post">
-        <div class="mb-3 row">
-            <label for="inputPassword" class="col-sm-2 col-form-label">Numero de compte</label>
-            <div class="col-sm-10">
-            <input type="text" name="code" class="form-control" id="inputPassword">
-            </div>
-        </div>
-        <div class="mb-3 row">
-            <label for="inputPassword" class="col-sm-2 col-form-label">Libelle</label>
-            <div class="col-sm-10">
-            <input type="text" name="libelle" class="form-control" id="inputPassword">
-            </div>
-        </div>
-        <center><div class="mb-3">
-            <button class="btn btn-primary" type="submit">Submit form</button>
-        </div></center>
-    </form>
-<hr>
-    <center><h1>Ou</h1></center>
-    <form action="<?php echo base_url('index.php/Insert_comptable/uploadcsv'); ?>" method="post"
-enctype='multipart/form-data'>
-        <div>
-            <center><label for="formFileLg" class="form-label">Inserer un fichier CSV</label></center>
-            <input class="form-control form-control-lg" id="formFileLg" type="file" name="file">
-        </div>
-        <br>
-        <center><div class="mb-3">
-            <button class="btn btn-primary" type="submit">Submit form</button>
-        </div></center>
-    </form>
+    <div class = 'row'>
+        <div class = 'col-3'></div>
+        <div class = 'col-6'>
+            <form action="<?php echo base_url('index.php/Insert_comptable/Insert'); ?>" method="post">
+                <div class="mb-3 row">
+                    <label for="inputPassword" class="col-sm-2 col-form-label">Numero de compte</label>
+                    <div class="col-sm-10">
+                    <input type="text" name="code" class="form-control" id="inputPassword">
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="inputPassword" class="col-sm-2 col-form-label">Libelle</label>
+                    <div class="col-sm-10">
+                    <input type="text" name="libelle" class="form-control" id="inputPassword">
+                    </div>
+                </div>
+                <center><div class="mb-3">
+                    <button class="btn btn-primary" type="submit">Submit form</button>
+                </div></center>
+            </form>
+        <hr>
+            <center><h1>Ou</h1></center>
+            <form action="<?php echo base_url('index.php/Insert_comptable/uploadcsv'); ?>" method="post" enctype='multipart/form-data'>
+                <div>
+                    <center><label for="formFileLg" class="form-label">Inserer un fichier CSV</label></center>
+                    <input class="form-control form-control-lg" id="formFileLg" type="file" name="file">
+                </div>
+                <br>
+                <center><div class="mb-3">
+                    <button class="btn btn-primary" type="submit">Submit form</button>
+                </div></center>
+            </form>
 
+        </div>
+        <div class = 'col-3'></div>
+        </div>
+    </div>
 </body>
 </html>
