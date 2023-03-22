7c26fa7968d5456fa3c032d3508bcc63807adafd
Liantsiky Rasoanaivo <liantsiky@pop-os.localdomain>
2023-03-22 03:40:44 +0300

    "Nouveau model GRand livre et Balance"

---
+++ application/models/GrandLivre_model.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+    
+    class GrandLivre_model extends CI_Model {
+            /**
+         * @param string $code is the 'compte' of the plancomptable
+         * @return array array of  the grand livre of the specified code
+         */
+        public function getOneGrangLivre($code){
+            $query = ("select * from getGrandlivre('%s')");
+            $query= sprintf($query,$code);
+            $query = $this->db->query($query);
+            $compte = array();
+
+            foreach($query->result_array() as $row){
+                $compte[] = $row;
+            }
+
+            return $compte;
+        }
+    }
+?>
