72ae7881df3577ec7d826c16ec83dfb752cb6ed6
Liantsiky Rasoanaivo <liantsiky@pop-os.localdomain>
2023-05-10 03:00:51 +0300

    "bilan 3"

--- script/baseSILiantsiky.sql
+++ script/baseSILiantsiky.sql
@@ -5,6 +5,31 @@
         from journal j
         join plancomptable c on j.idPlanComptable=c.idPlanComptable
     -- maka compte
+        --passif
+        --101
+        CREATE OR REPLACE VIEW v_get_101 as
+            SELECT *
+                FROM v_tableau j where codejournal LIKE '101%';
+        --106
+        CREATE OR REPLACE VIEW v_get_106 as
+            SELECT *
+                FROM v_tableau j where codejournal LIKE '106%';
+        --11
+        CREATE OR REPLACE VIEW v_get_11 as
+            SELECT *
+                FROM v_tableau j where codejournal LIKE '11%';
+        --128
+        CREATE OR REPLACE VIEW v_get_128 as
+            SELECT *
+                FROM v_tableau j where codejournal LIKE '128%';
+        --161
+        CREATE OR REPLACE VIEW v_get_161 as
+            SELECT *
+                FROM v_tableau j where codejournal LIKE '161%';
+        --165
+        CREATE OR REPLACE VIEW v_get_165 as
+            SELECT *
+                FROM v_tableau j where codejournal LIKE '165%';
         -- actif brut
         --20
         CREATE OR REPLACE VIEW v_get_20 as
@@ -34,10 +59,10 @@
         CREATE OR REPLACE VIEW v_get_3 as
             SELECT *
                 FROM v_tableau j where codejournal LIKE '3%' AND codejournal not like '39%';
-        --41
+        --4
         CREATE OR REPLACE VIEW v_get_4 as
             SELECT *
-                FROM v_tableau j where codejournal LIKE '4%' AND codejournal not like '41%';
+                FROM v_tableau j where codejournal LIKE '4%' AND codejournal not like '41%' AND codejournal not like '49%';
         --41
         CREATE OR REPLACE VIEW v_get_41 as
             SELECT *
@@ -55,13 +80,29 @@
         CREATE OR REPLACE VIEW v_get_281 as
             SELECT *
                 FROM v_tableau j where codejournal LIKE '281%';
+        --282
+        CREATE OR REPLACE VIEW v_get_282 as
+            SELECT *
+                FROM v_tableau j where codejournal LIKE '282%';
+        --283
+        CREATE OR REPLACE VIEW v_get_283 as
+            SELECT *
+                FROM v_tableau j where codejournal LIKE '283%';
+        --285
+        CREATE OR REPLACE VIEW v_get_285 as
+            SELECT *
+                FROM v_tableau j where codejournal LIKE '285%';
         --39
         CREATE OR REPLACE VIEW v_get_39 as
             SELECT *
                 FROM v_tableau j where codejournal LIKE '39%';
+        --49
+        CREATE OR REPLACE VIEW v_get_49 as
+            SELECT *
+                FROM v_tableau j where codejournal LIKE '49%'  ;
         --491
         CREATE OR REPLACE VIEW v_get_491 as
             SELECT *
                 FROM v_tableau j where codejournal LIKE '491%';
 
-                
+        
