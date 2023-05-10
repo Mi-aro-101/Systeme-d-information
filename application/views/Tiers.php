35d4387b95063fcc0ce6c8641d91ce706701f3df
Mi-aro-101 <119170091+Mi-aro-101@users.noreply.github.com>
2023-05-10 05:53:33 -0400

    Update Tiers.php

--- application/views/Tiers.php
+++ application/views/Tiers.php
@@ -1,5 +1,4 @@
-<!-- partial -->
-      <div class="main-panel">
+<div class="main-panel">
         <div class="content-wrapper">
           <div class="row">
             <div class="col-md-12 grid-margin stretch-card">
@@ -9,24 +8,22 @@
                   <div class="row">
                     <div class="col-12">
                       <div class="table-responsive">
-                        <table id="example" class="display expandable-table" style="width:100%">
+                        <table style="width:100%">
                           <thead>
-                            <tr>
-                                <th>Numero</th>
-                                <th>Intitule du compte</th>
-                                <th>Suprrimer</th>
-                                <th>Modifier</th>
-                            </tr>
+                            <th>Numero</th>
+                            <th>Intitule du compte</th>
+                            <th>Suprrimer</th>
+                            <th>Modifier</th>
                           </thead>
-                          <?php foreach($table as $tab){?>
+                            <?php foreach($table as $tab){?>
                             <tr>
-                                <td><?php echo $tab['numerocompte']; ?></td>
-                                <td><?php echo $tab['intitule']; ?></td>
-                                <td><a href=<?php echo base_url('index.php/Tiers_interaction/Supprimer/'.$tab["idplantiers"].'');?>><button class="btn btn-danger"><i class="bi bi-trash3-fill"></i></button class="btn btn-danger"></a></td>
-                                <td><a href="<?php echo base_url('index.php/Tiers_interaction/Modifier/'.$tab["idplantiers"].'');?>"><button class="btn btn-warning"><i class="bi bi-pencil-fill"></button class="btn btn-warning"></a></td>
+                                    <td><?php echo $tab['numerocompte']; ?></td>
+                                    <td><?php echo $tab['intitule']; ?></td>
+                                    <td><a href=<?php echo base_url('index.php/Tiers_interaction/Supprimer/'.$tab["idplantiers"].'');?>><button class="btn btn-danger"><i class="bi bi-trash3-fill"></i></button class="btn btn-danger"></a></td>
+                                    <td><a href="<?php echo base_url('index.php/Tiers_interaction/Modifier/'.$tab["idplantiers"].'');?>"><button class="btn btn-warning"><i class="bi bi-pencil-fill"></button class="btn btn-warning"></a></td>
                             </tr>
-                         <?php } ?>
-                        </table>    
+                            <?php } ?>
+                      </table>
                       </div>
                     </div>
                   </div>
@@ -35,5 +32,15 @@
               </div>
             </div>
         </div>
-        </div>
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
+        <!-- partial -->
       </div>
+      <!-- main-panel ends -->
+    </div>   
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+</body>
+
+</html>
