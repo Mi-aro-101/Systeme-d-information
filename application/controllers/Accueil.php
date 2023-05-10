517966b2fd734f9c315c531defe07441230016d5
Mi-aro-101 <miaroramanandraitsiory@gmail.com>
2023-05-10 04:56:09 -0400

    Nasian'i git zavatra masiso be dia nesorina fa nampisy lagy

--- application/controllers/Accueil.php
+++ application/controllers/Accueil.php
@@ -5,12 +5,9 @@ class Accueil extends CI_Controller {
 
 
 	public function index(){
-<<<<<<< HEAD
         // $this->load->view('Template');
         $this->template->write('title','Accueil',TRUE);
         $this->template->write_view('content','Accueil','',TRUE);
-=======
         //$this->load->view('Template');
->>>>>>> 3ccd9ab1a667b77f5b6fe2fba95f4f8f3ca1948f
     }
 }
--- application/controllers/Bilan_interaction.php
+++ application/controllers/Bilan_interaction.php
@@ -16,19 +16,10 @@ class Bilan_interaction extends CI_Controller {
         $data['actifBrut'] = $actifBrut;
         $data['actifNet'] = $actifNet;
         $data['passif'] = $passif;
-        // return $data;
-<<<<<<< HEAD
-        // $this->load->view('Template');
-        // $this->load->view('Actif_result',$data);
-        // $this->load->view('Passif_result',$data);
+
         $this->template->write('title','Bilan',TRUE);
         $this->template->write_view('content','Actif_result',$data);
         $this->template->write_view('content','Passif_result',$data);
         $this->template->render();
-=======
-        //$this->load->view('Template');
-        //$this->load->view('Actif_result',$data);
-        //$this->load->view('Passif_result',$data);
->>>>>>> 3ccd9ab1a667b77f5b6fe2fba95f4f8f3ca1948f
     }
 }
